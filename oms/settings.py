0fb36e384a4bab2f0a5a65c04edd2c7cdf230579
harisankar <harisankar9633@gmail.com>
2022-01-23 12:14:21 +0530

    first commit-1

--- oms/settings.py
+++ oms/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'oms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oms',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': '1234',
     }
 }
 
@@ -116,9 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Products
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def products(request):
+    product_list = Products.objects.all()
+    return render(request, 'products.html', {'product_list': product_list})
