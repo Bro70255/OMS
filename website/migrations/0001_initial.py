faaa03c33ab40862934d7345abceb02261b97bad
harisankar <harisankar9633@gmail.com>
2022-01-23 19:51:53 +0530

    first commit-4

--- website/migrations/0001_initial.py
+++ website/migrations/0001_initial.py
@@ -21,4 +21,5 @@ class Migration(migrations.Migration):
                 ('price', models.IntegerField()),
             ],
         ),
+
     ]
--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.products, name='products'),
     path('checkout/', views.checkout, name='checkout'),
+    path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
 ]
