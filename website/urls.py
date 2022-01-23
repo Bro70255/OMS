902b3af896e1820cc14d2f076ba3256b73e5dac7
harisankar <harisankar9633@gmail.com>
2022-01-23 22:35:28 +0530

    first commit-5

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products, name='products'),
+    path('', views.index, name='index'),
     path('checkout/', views.checkout, name='checkout'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('products/', views.products, name='products'),
 ]
